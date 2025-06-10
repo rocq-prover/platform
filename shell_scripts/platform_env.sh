754b38a21e140f97efafd26a5dfcd4d44f8a0d82
Sylvain Borgogno <sylvain.borgogno@inria.fr>
2025-06-10 10:50:49 +0200

    Fix: change CI to retrieve COQ_PLATFORM_PACKAGE_PICK_FILE and define major version

--- shell_scripts/platform_env.sh
+++ shell_scripts/platform_env.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ###################### COPYRIGHT/COPYLEFT ######################
-
 # (C) 2025 Sylvain Borgogno
-
 # Released to the public under the
 # Creative Commons CC0 1.0 Universal License
 # See https://creativecommons.org/publicdomain/zero/1.0/legalcode.txt
 
+if [ -z "${COQ_PLATFORM_PACKAGE_PICK_FILE:-}" ]; then
+  echo "Error: COQ_PLATFORM_PACKAGE_PICK_FILE is not defined"
+  exit 1
+else
+  echo "Package pick file detected : $COQ_PLATFORM_PACKAGE_PICK_FILE"
+fi
+
 if [ -z "${COQ_PLATFORM_VERSION_MAJOR:-}" ]; then
   if [[ $COQ_PLATFORM_PACKAGE_PICK_FILE =~ package-pick-([0-9]+)\. ]]; then
     export COQ_PLATFORM_VERSION_MAJOR="${BASH_REMATCH[1]}"
+    echo " : $COQ_PLATFORM_VERSION_MAJOR"
   else
-    echo "Impossible to extract major version from '$COQ_PLATFORM_PACKAGE_PICK_FILE'"
+    echo "Impossible to extract Major version '$COQ_PLATFORM_PACKAGE_PICK_FILE'"
     exit 1
   fi
-  echo "Major version detected : $COQ_PLATFORM_VERSION_MAJOR"
+else
+  echo "COQ_PLATFORM_VERSION_MAJOR already defined : $COQ_PLATFORM_VERSION_MAJOR"
 fi
