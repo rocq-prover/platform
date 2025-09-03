0ca4521c6dcce74cbf0dd5c1dee31512cf6c8e63
Sylvain Borgogno <sylvain.borgogno@inria.fr>
2025-09-03 12:47:07 +0200

    test: update rocq-hierarchy-builder to 1.10.0 to test with Rocq 9.0

--- package_picks/package-pick-9.0+preview~2025.08.sh
+++ package_picks/package-pick-9.0+preview~2025.08.sh
@@ -79,7 +79,7 @@ then
 
   # General mathematics
   PACKAGES="${PACKAGES} elpi.3.1.0 rocq-elpi.3.1.0" # according to Enrico's wishes
-  PACKAGES="${PACKAGES} rocq-hierarchy-builder.1.9.1"
+  PACKAGES="${PACKAGES} rocq-hierarchy-builder.1.10.0"
   PACKAGES="${PACKAGES} coq-mathcomp-ssreflect.2.4.0"
   PACKAGES="${PACKAGES} coq-mathcomp-fingroup.2.4.0"
   PACKAGES="${PACKAGES} coq-mathcomp-algebra.2.4.0"
