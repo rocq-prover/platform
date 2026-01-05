8e17d1ede3edd2cdc527394a7065029844f5b6df
Sylvain Borgogno <sylvain.borgogno@inria.fr>
2026-01-05 22:17:12 +0100

    fix: fix name of COQ_PLATFORM_PACKAGE_PICK_POSTFIX

--- package_picks/package-pick-9.0~2025.08.sh
+++ package_picks/package-pick-9.0~2025.08.sh
@@ -18,7 +18,7 @@ COQ_PLATFORM_VERSION_SORTORDER="1"
 # It is usually either empty ot starts with ~.
 # It might also be used for installer package names, but with ~ replaced by _
 # It is also used for version specific file selections in the smoke test kit.
-COQ_PLATFORM_PACKAGE_PICK_POSTFIX="~9.0.1~2025.08"
+COQ_PLATFORM_PACKAGE_PICK_POSTFIX="~9.0~2025.08"
 
 # The corresponding Coq development branch and tag
 COQ_PLATFORM_COQ_BRANCH="v9.0.1"
